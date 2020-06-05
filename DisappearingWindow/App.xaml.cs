a18c6cd9d2cf010248fb98dcd46b8276d290ff50
TSK <tomasz.skowron@soloplan.pl>
2020-06-05 10:13:53 +0200

    Add project files.

---
+++ DisappearingWindow/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DisappearingWindow
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+  }
+}
---
+++ DisappearingWindow/ConfigPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DisappearingWindow
+{
+  /// <summary>
+  /// Interaction logic for ConfigPage.xaml
+  /// </summary>
+  public partial class ConfigPage : Page, INotifyPropertyChanged
+  {
+    public ConfigPage()
+    {
+      
+      InitializeComponent();
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private async void AddConnectorClick(object sender, RoutedEventArgs e)
+    {
+
+      var dialog = new DialogPopup();
+
+      var result = (bool)await DialogHost.Show(dialog, "ConfigPageDialogHost");
+      if(result)
+      {
+        return;
+      }
+      else
+      {
+        return;
+      }
+    }
+
+    private void PageInitialized(object sender, EventArgs e)
+    {
+
+    }
+
+
+  }
+}
